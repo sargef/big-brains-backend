74cffe833d4f417b01135d51cbe4acba87a8958b
Sarge <35126840+sargef@users.noreply.github.com>
2019-06-02 19:30:03 +0930

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-dimensional-79250',
     user : 'aneagoie',
     password : '',
     database : 'smart-brain'
